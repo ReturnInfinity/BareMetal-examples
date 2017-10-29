37131b9fc7192eab80e72b033f28dc84344b31b6
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-29 14:17:41 -0400

    added hello world example for C

---
+++ hello-c.c
@@ -0,0 +1,15 @@
+/* A 'Hello, world' example for C.
+ * Written by Taylor Holberton
+ *
+ * Compile: 'gcc hello-c.c -o hello-c'
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	puts("Hello, world!");
+	return EXIT_SUCCESS;
+}
+
