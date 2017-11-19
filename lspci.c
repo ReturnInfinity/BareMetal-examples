1b67cc37c88c55f34f7f96c36aa68db6cb2fe35d
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-19 13:08:45 -0500

    using BareMetal system call instead of inline assembly

--- lspci.c
+++ lspci.c
@@ -110,40 +110,6 @@ static void print_u16(uint16_t n)
 	print_u8((n >> 0x00) & 0xff);
 }
 
-static void sysOut32(uint32_t port, uint32_t data)
-{
-	asm volatile("outl %0, %w1" : : "a" (data), "Nd" (port));
-}
-
-static uint32_t sysIn32(uint32_t port)
-{
-	uint32_t data;
-	asm volatile("inl %w1, %0" : "=a" (data) : "Nd" (port));
-	return data;
-}
-
- uint16_t pciConfigReadWord(uint8_t bus, uint8_t slot,
-                            uint8_t func, uint8_t offset)
-{
-	uint32_t address;
-	uint32_t lbus  = (uint32_t)bus;
-	uint32_t lslot = (uint32_t)slot;
-	uint32_t lfunc = (uint32_t)func;
-	uint16_t tmp = 0;
- 
-	// create configuration address
-	address = (uint32_t)((lbus << 16) | (lslot << 11) |
-	          (lfunc << 8) | (offset & 0xfc) | ((uint32_t)0x80000000));
- 
-	// write out the address
-	sysOut32 (0xCF8, address);
-	// read in the data
-	// (offset & 2) * 8) = 0 will choose the first word of the 32 bits register
-	tmp = (uint16_t)((sysIn32 (0xCFC) >> ((offset & 2) * 8)) & 0xffff);
-
-	return tmp;
-}
-
 int iterate_pci(void *fdata, int (*f)(void *fdata, const struct pci_entry *entry))
 {
 	struct pci_entry entry;
@@ -170,9 +136,9 @@ int iterate_pci(void *fdata, int (*f)(void *fdata, const struct pci_entry *entry
  uint16_t pciCheckVendor(uint8_t bus, uint8_t slot)
 {
 	uint16_t vendor, device;
-	if ((vendor = pciConfigReadWord(bus, slot, 0, 0)) != 0xffff) {
+	if ((vendor = b_pci_read_config(bus, slot, 0, 0)) != 0xffff) {
 		// found a device
-		device = pciConfigReadWord(bus, slot, 0, 2);
+		device = b_pci_read_config(bus, slot, 0, 2);
 		(void) device;
 	}
 	return vendor;
